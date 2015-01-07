bc546f1ed3e7ffdbbcedb2738832ffd86fd6ba46
Andrew Snead <andrewcsnead@gmail.com>
2015-01-08 01:05:43 +1100

    use ++ for increment like c

--- ForDemo.java
+++ ForDemo.java
@@ -2,7 +2,7 @@ class ForDemo {
   public static void main(String args[]) {
     int count;
 
-    for(count = 0; count < 5; count = count+1)
+    for(count = 0; count < 5; count++)
       System.out.println("this is count: " + count);
 
     System.out.println("Done!");

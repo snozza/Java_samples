b4a6ba78d2609c98d770249f87de21d30686b1e3
Andrew Snead <andrewcsnead@gmail.com>
2015-01-08 00:10:30 +1100

    made a hello world java program

---
+++ example.java
@@ -0,0 +1,9 @@
+/* example Java program
+*/
+
+class Example {
+  // A java program begins with a call to main()
+  public static void main(String args[]) {
+    System.out.println("Java drives the Web.");
+  }
+}

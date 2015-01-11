ec915ced1d514d235ac51d5dc4aa2481be8b60f5
Andrew Snead <andrewcsnead@gmail.com>
2015-01-12 01:17:32 +1100

    add demo for recursion

--- Factorial.java
+++ Factorial.java
@@ -8,3 +8,14 @@ class Factorial {
     return result;
   }
 }
+
+class Recursion {
+  public static void main(String args[]) {
+    Factorial f = new Factorial();
+
+    System.out.println("Factorials using recusive method.");
+    System.out.println("Factorial of 3 is " + f.factR(3));
+    System.out.println("Factorial of 4 is " + f.factR(4));
+    System.out.println();
+  }
+}

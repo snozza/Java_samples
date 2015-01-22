64674eb1635b4402268371298b2eeadffd9ff839
Andrew Snead <andrewcsnead@gmail.com>
2015-01-22 13:11:29 +0000

    restructured folders

---
+++ bookpackext/UseBook.java
@@ -0,0 +1,20 @@
+package bookpackext;
+
+class UseBook {
+  public static void main(String args[]) {
+    bookpack.Book books[] = new bookpack.Book[5];
+
+    books[0] = new bookpack.Book("Java: A Beginner's Guide",
+                                 "Schildt", 2014);
+    books[1] = new bookpack.Book("Java: The Complete reference",
+                                 "Schildt", 2014);
+    books[2] = new bookpack.Book("The art of Java",
+                                 "Schildt and Holmes", 2003);
+    books[3] = new bookpack.Book("Red Storm rising", 
+                                 "Clancy", 1986);
+    books[4] = new bookpack.Book("On the Road", 
+                                 "Kerouac", 1955);
+
+    for(int i = 0; i < books.length; i++) books[i].show();
+  }
+}

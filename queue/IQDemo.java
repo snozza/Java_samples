67632db6d0365f4eb11e107d6cfdb10a06a0ab9e
Andrew Snead <andrewcsnead@gmail.com>
2015-01-23 12:25:07 +0100

    Added dynamic Queue to resize array

--- queue/IQDemo.java
+++ queue/IQDemo.java
@@ -59,3 +59,35 @@ class CircularQueue implements ICharQ {
     return ch;
   }
 }
+
+class DynQueue implements ICharQ {
+  private char q[];
+  private int putloc, getloc;
+
+  public DynQueue(int size) {
+    q = new char[size];
+    putloc = getloc = 0;
+  }
+
+  public void put(char ch) {
+    if(putloc == q.length) {
+      char t[] = new char[q.length * 2];
+
+      for(int i=0; i < q.length; i++)
+        t[i] = q[i];
+
+      q = t;
+    }
+
+    q[putloc++] = ch;
+  }
+
+  public char get() {
+    if(getloc == putloc) {
+      System.out.println(" - Queue is empty");
+      return (char) 0;
+    }
+
+    return q[getloc++];
+  }
+}

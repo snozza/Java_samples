405c60fc44548d3eb35f9186d7f3528c05a22fa2
Andrew Snead <andrewcsnead@gmail.com>
2015-01-23 13:24:04 +0100

    sample queue class added

--- queue/IQDemo.java
+++ queue/IQDemo.java
@@ -91,3 +91,78 @@ class DynQueue implements ICharQ {
     return q[getloc++];
   }
 }
+
+class IQDemo {
+  public static void main(String args[]) {
+    FixedQueue q1 = new FixedQueue(10);
+    DynQueue q2 = new DynQueue(5);
+    CircularQueue q3 = new CircularQueue(10);
+
+    ICharQ iQ;
+
+    char ch;
+    int i;
+
+    iQ = q1;
+
+    for(i = 0; i < 10; i++)
+      iQ.put((char) ('A' + i));
+
+    System.out.print("contents of fixed queue: ");
+    for(i = 0; i < 10; i++) {
+      ch = iQ.get();
+      System.out.print(ch);
+    }
+    System.out.println();
+
+    iQ = q2;
+    for(i = 0; i < 10; i++) 
+      iQ.put((char) ('Z' - i));
+
+    System.out.print("Contents of dynamic queue: ");
+    for(i = 0; i < 10; i++) {
+      ch = iQ.get();
+      System.out.print(ch);
+    }
+
+    System.out.println();
+
+    iQ = q3;
+    for(i = 0; i < 10; i++)
+      iQ.put((char) ('A' + i));
+
+    System.out.println("Contents of the circular Queue: ");
+    for(i = 0; i < 10; i++) {
+      ch = iQ.get();
+      System.out.print(ch);
+    }
+
+    System.out.println();
+
+    for(i = 10; i < 20; i++)
+      iQ.put((char) ('A' + i));
+
+    System.out.print("Contents of circular queue: ");
+    for(i = 0; i < 10; i++) {
+      ch = iQ.get();
+      System.out.print(ch);
+    }
+
+    System.out.println();
+
+    System.out.println("\nStore and consume from" +
+                       " circular queue.");
+
+    for(i = 0; i < 10; i++) {
+      iQ.put((char) ('A' + i));
+      ch = iQ.get();
+      System.out.print(ch);
+    }
+
+    System.out.println();
+  }
+}
+
+
+
+

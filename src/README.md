3b09ae99d3acb1b6a285a8fc084ba1fb9776b5fc
Andrew Snead <andrewcsnead@gmail.com>
2015-01-24 16:52:51 +0100

    removing simple sample files

--- src/README.md
+++
@@ -1,4 +0,0 @@
-# Java_samples
-Java sample programs, design patterns and simple algorithms.
-
-Purely for learning Java syntax.
--- src/example.java
+++
@@ -1,9 +0,0 @@
-/* example Java program
-*/
-
-class Example {
-  // A java program begins with a call to main()
-  public static void main(String args[]) {
-    System.out.println("Java drives the Web.");
-  }
-}

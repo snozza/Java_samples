ab11133d2c7d27d55b1c0c164a3f4e7d42a1bd69
Andrew Snead <andrewcsnead@gmail.com>
2015-01-25 21:26:20 +0000

    Multi threaded example

--- threads/MyThread.java
+++ threads/MyThread.java
@@ -1,23 +1,24 @@
 class MyThread implements Runnable {
-  String thrdName;
+  Thread thrd;
 
   MyThread(String name) {
-    thrdName = name;
+    thrd = new Thread(this, name);
+    thrd.start();
   }
 
   public void run() {
-    System.out.println(thrdName + " starting.");
+    System.out.println(thrd.getName() + " starting.");
     try {
       for(int count=0; count < 10; count++) {
         Thread.sleep(400);
-        System.out.println("In " + thrdName +
+        System.out.println("In " + thrd.getName() +
                            ", count is " + count);
       }
     }
     catch(InterruptedException exc) {
-      System.out.println(thrdName + " interrupted.");
+      System.out.println(thrd.getName() + " interrupted.");
     }
-    System.out.println(thrdName + " terminating.");
+    System.out.println(thrd.getName() + " terminating.");
   }
 }
 
@@ -26,10 +27,6 @@ class UseThreads {
     System.out.println("Main thread starting.");
 
     MyThread mt = new MyThread("Child #1");
-
-    Thread newThrd = new Thread(mt);
-
-    newThrd.start();
 
     for(int i=0; i < 50; i++) {
       System.out.print(".");
@@ -41,6 +38,27 @@ class UseThreads {
       }
     }
 
+    System.out.println("Main thread ending.");
+  }
+}
+
+class MoreThreads {
+  public static void main(String args[]) {
+    System.out.println("Main thread starting.");
+
+    MyThread mt1 = new MyThread("Child #1");
+    MyThread mt2 = new MyThread("Child #2");
+    MyThread mt3 = new MyThread("Child #3");
+
+    for(int i=0; i < 50; i++) {
+      System.out.print(".");
+      try {
+        Thread.sleep(100);
+      }
+      catch(InterruptedException exc) {
+        System.out.println("Main thread interrupted.");
+      }
+    }
     System.out.println("Main thread ending.");
   }
 }
